98e283b6c412431bcd83f7248953873094c0227a
kiwi <21130504@st.hcmuaf.edu.vn>
2025-05-09 12:34:03 +0700

    fix api register

--- src/main/java/com/example/OT/Doctor/Booking/Enum/Role.java
+++ src/main/java/com/example/OT/Doctor/Booking/Enum/Role.java
@@ -2,7 +2,7 @@ package com.example.OT.Doctor.Booking.Enum;
 
 public enum Role {
     // Phải có prefix "ROLE_" để tương thích Spring Security
-    ROLE_PATIENT,
-    ROLE_DOCTOR,
-    ROLE_ADMIN;
+    PATIENT,
+    DOCTOR,
+    ADMIN;
 }
