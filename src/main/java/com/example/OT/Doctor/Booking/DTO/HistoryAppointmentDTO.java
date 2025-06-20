1017dbc6aaa4bd3f3748d036d24e4b1eba9cb6d8
Thien-Nhan-28 <thiennhanyahoo@gmail.com>
2025-06-20 22:15:56 +0700

    update api history booking

--- src/main/java/com/example/OT/Doctor/Booking/DTO/HistoryAppointmentDTO.java
+++ src/main/java/com/example/OT/Doctor/Booking/DTO/HistoryAppointmentDTO.java
@@ -1,23 +1,19 @@
 package com.example.OT.Doctor.Booking.DTO;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.time.ZoneId;
-import java.util.Date;
 
 public class HistoryAppointmentDTO {
     private Integer appointmentNumber;
     private LocalDate appointmentDate;
     private LocalTime appointmentTime;
 
-    public HistoryAppointmentDTO(Integer appointmentNumber, LocalDateTime appointmentDateTime) {
+    public HistoryAppointmentDTO(Integer appointmentNumber, LocalDate appointmentDate, LocalTime appointmentTime) {
         this.appointmentNumber = appointmentNumber;
-        this.appointmentDate = appointmentDateTime.toLocalDate();
-        this.appointmentTime = appointmentDateTime.toLocalTime().withSecond(0).withNano(0);
+        this.appointmentDate = appointmentDate;
+        this.appointmentTime = appointmentTime.withSecond(0).withNano(0);
     }
 
-    // Getters
     public Integer getAppointmentNumber() {
         return appointmentNumber;
     }
